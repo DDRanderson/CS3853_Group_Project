284c7b9aaa277aaadbed014da6cdee520fc92422
DDRanderson <95324842+DDRanderson@users.noreply.github.com>
2024-11-15 12:08:59 -0600

    inst and dstsrc bytes being read correctly

--- driver.java
+++ driver.java
@@ -1,7 +1,9 @@
+import java.io.BufferedReader;
 import java.io.File;
 import java.util.ArrayList;
 import java.text.DecimalFormat;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintStream;
 
@@ -14,7 +16,7 @@ public class driver {
 			System.exit(0);
 		}
 		
-		// variables
+		// cache global variables
 		int cacheSize = 0;
 		int blockSize = 0;
 		int assoc = 0;
@@ -23,6 +25,7 @@ public class driver {
 		int timeSlice = 0;
 		String policy = new String();
         ArrayList<File> fileList = new ArrayList<>();
+		ArrayList<Tracefile> traceFileList = new ArrayList<>();
 		
         // stores arguments into respective variables
 		for (int i = 0; i < args.length; i++) {
@@ -56,7 +59,10 @@ public class driver {
 					break;
 					
 				case "-f":
-					fileList.add(new File(args[++i]));
+					File f = new File(args[++i]);
+					fileList.add(f);
+					traceFileList.add(new Tracefile(f));
+					//traceFileList.add(new Tracefile(f.getName()));
 					break;
 				
 				default:
@@ -64,14 +70,18 @@ public class driver {
 			}
 		}
 		
-		//print all to screen and output file
+		//////////////////////////////////
+		//		BEGIN MILESTONE #1		//
+		//////////////////////////////////
+
+		//print all to information to the screen and write to and output file
 		System.out.println("Cache Simulator - CS 3853 - Group #05\n");
 		printTraceFiles(fileList);
 		printInputParams(cacheSize, blockSize, assoc, physMem, memUsed, timeSlice, policy);
 		printCacheCalcs(cacheSize, blockSize, assoc, physMem, memUsed, timeSlice, policy);
 		printPhysicalMemoryCalcs(physMem, memUsed, fileList.size());
 		
-		try (PrintStream out = new PrintStream(new FileOutputStream("Team_05_Sim_n_M#1.txt"))){
+		/*try (PrintStream out = new PrintStream(new FileOutputStream("Team_05_Sim_n_M#1.txt"))){
 			System.setOut(out);
 			System.out.println("Cache Simulator - CS 3853 - Group #05\n");
 			printTraceFiles(fileList);
@@ -80,7 +90,128 @@ public class driver {
 			printPhysicalMemoryCalcs(physMem, memUsed, fileList.size());
 		} catch (IOException e) {
 			e.printStackTrace();
+		}*/
+		//////////////////////////////////
+		//		END MILESTONE #1		//
+		//////////////////////////////////
+
+
+
+		//////////////////////////////////
+		//       BEGIN MILESTONE #2		//
+		//////////////////////////////////
+		
+		//setup the cache
+		//each block set to -1 to indicate valid is not set/no tag written
+		int[][] arrCache = new int[CalculateNumOfSets(cacheSize,blockSize,assoc)][assoc];
+			for (int row = 0; row < arrCache.length; row++){
+				for (int col = 0; col < arrCache[row].length; col++){
+					arrCache[row][col] = -1;
+				}
+			}
+		
+		//process each trace file, save info to variables and cache
+		int totalAddressesRead = 0;			//+1 EIP address & +1src & +1dst (if src/dst read occurred) 
+		int sumInstructionBytes = 0;		//sum of all the numbers in (_) after EIP
+		int sumDstSrcBytes = 0;			//if src/dst read occurred, add 4 for either one where read occurred
+
+		int doneCount = 0;
+		for (int i = 0; doneCount < traceFileList.size(); i++){
+			Tracefile currentFile = traceFileList.get(i);
+
+			//loop back to beginning of trace files array
+			if(i == traceFileList.size()) {
+				i = 0;
+			}
+
+			if(currentFile.isDoneReading) {
+				continue;
+			}
+		
+			try
+			{
+				BufferedReader br = new BufferedReader(new FileReader(currentFile.filePath));
+				String line = null;
+				char[] charArray = new char[100];
+
+				
+				//reads every line in the file
+				//TODO: need to adjust for TimeSlice
+				while (!currentFile.isDoneReading)
+				{
+					line = br.readLine();
+					if (line == null){
+						currentFile.isDoneReading = true;
+						doneCount++;
+						break;
+					}
+					//convert line to a char array
+					charArray = line.toCharArray();
+					if(charArray.length != 0)
+					{
+						switch(charArray[0])
+						{
+							//process the instruction address, hex address is [10-17]
+							case 'E':
+								String eipNum = new StringBuilder().append(charArray[5]).append(charArray[6]).toString();
+								sumInstructionBytes += Integer.parseInt(eipNum);
+								StringBuilder sbEipAddress = new StringBuilder();
+								for (int j = 10; j <= 17; j++)
+								{
+									sbEipAddress.append(charArray[j]);
+								}
+								String eipAddress = sbEipAddress.toString();
+								//TODO: send the eip address for cache hit/miss check
+								totalAddressesRead++;
+								break;
+							
+							//process both dst[6-13][15-22] and src[33-40][42-49] addresses
+							case 'd':
+								//dstM:
+								String dstAddress = null;
+								StringBuilder sbDst = new StringBuilder();
+								for (int j = 6; j <= 13; j++)
+								{
+									sbDst.append(charArray[j]);
+								}
+								dstAddress = sbDst.toString();
+								if (!dstAddress.equals("00000000"))
+								{
+									//TODO: send dst address for cache hit/miss check
+									sumDstSrcBytes += 4;
+									totalAddressesRead++;
+								}
+
+								//srcM:
+								String srcAddress = null;
+								StringBuilder sbSrc = new StringBuilder();
+								for (int j = 33; j <= 40; j++)
+								{
+									sbSrc.append(charArray[j]);
+								}
+								srcAddress = sbSrc.toString();
+								if (!srcAddress.equals("00000000"))
+								{
+									//TODO: send src address for cache hit/miss check
+									sumDstSrcBytes += 4;
+									totalAddressesRead++;
+								}
+
+							default:
+								break;
+						}
+					}
+				}
+				br.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
+		//print cache results
+		System.out.println("\n*****CACHE SIMULATION RESULTS*****\n");
+		System.out.println("Total Cache Accesses: \t" + "------" + "\t(" + totalAddressesRead + " addresses)");
+		System.out.print("Instruction Bytes: \t" + sumInstructionBytes);
+		System.out.println("\tSrcDst Bytes: " + sumDstSrcBytes);
 	}
 	
 	
@@ -193,4 +324,32 @@ public class driver {
     public static int CalculateNumOfSets(int cacheSize, int blockSize, int assoc){
         return cacheSize / (blockSize * assoc);
     }
+	
+	//Tracefile Class
+	public static class Tracefile{
+		public File filePath = null;
+		public String name = null;
+		public int currReadPos;
+		public boolean isDoneReading;
+		
+		public Tracefile(File filePath){
+			this.filePath = filePath;
+			currReadPos = 0;
+			isDoneReading = false;
+		}
+
+		/*public Tracefile(String name){
+			name = this.name;
+			filePath = new File(name);
+			currReadPos = 0;
+			isDoneReading = false;
+		}*/
+		
+		public String toString() {
+			return 	"Filepath " + filePath.getName()
+				+	"Name " + name
+				+	"currReadPos " + currReadPos
+				+ 	"Done Status " + Boolean.toString(isDoneReading);
+		}
+	}
 }	
